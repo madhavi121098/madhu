5c12ff8c3d59ad6b87e6c67c36ea965e765a5197
madhavi <madhulakshmi996@gmail.com>
2022-10-17 22:18:39 +0530

    updated my code

---
+++ Samsung.java
@@ -0,0 +1,10 @@
+class  Samsung
+{
+String cam="Camera";
+boolean flash=true;
+boolean isTouchDisp=false;
+void keyboard()
+{
+System.out.println("Samsung phones available at every price range");
+}
+}
