39b8ace6fd85d63419e7bc2f1c73115c71642964
madhavi <madhulakshmi996@gmail.com>
2022-10-19 10:54:11 +0530

    update

---
+++ Samsungwatch.java
@@ -0,0 +1,10 @@
+class Samsungwatch extends Samsung
+{
+String model="watch4";
+String Design="Excellent Design";
+void game()
+{
+System.out.println("playing games");
+System.out.println("long-lasting battery");
+}
+}
