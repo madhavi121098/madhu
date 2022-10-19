6acc724409fe755bfa143dbfb9c41be3ea99cebe
madhavi <madhulakshmi996@gmail.com>
2022-10-19 10:15:57 +0530

    update code

---
+++ Samsungm31.java
@@ -0,0 +1,11 @@
+class Samsungm31 extends Samsung
+{
+String model="Samsung m31";
+boolean touchscreen=true;
+void display()
+{
+System.out.println( "four camera at back side body with 64 MP");
+System.out.println( "Good quality display");
+System.out.println( "Fantastic viewing angle");
+} 
+}
